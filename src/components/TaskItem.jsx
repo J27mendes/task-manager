7018645734039ea90193b5acf5356a5ffe0110f4
J27mendez <mendes_j27@hotmail.com>
2025-02-24 16:34:50 -0300

    feat: ensure Taskitem changes depending on task status

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,22 +1,50 @@
+import Check from '../assets/icons/check.svg?react'
+import Loader from '../assets/icons/loader.svg?react'
+import Closed from '../assets/icons/closed.svg?react'
+import Details from '../assets/icons/details.svg?react'
+
 const TaskItem = ({ task }) => {
   const getStatusClasses = () => {
     if (task.status === 'done') {
-      return 'bg-[#00AD85] bg-opacity-10 text-[#00AD85]'
+      return 'bg-[#00AD85] text-[#00AD85]'
     }
 
     if (task.status === 'in_progress') {
-      return 'bg-[#FFAA04] bg-opacity-10 text-[#FFAA04]'
+      return 'bg-[#FFAA04] text-[#FFAA04]'
     }
 
     if (task.status === 'not_started') {
-      return 'bg-[#fE5A99] bg-opacity-10 text-[#fE5A99]'
+      return 'bg-[#fE5A99] text-[#fE5A99]'
     }
   }
   return (
     <div
-      className={`flex items-center gap-2 rounded-lg px-4 py-3 text-sm ${getStatusClasses()}`}
+      className={`flex items-center justify-between rounded-lg bg-opacity-10 px-4 py-3 text-sm ${getStatusClasses()}`}
     >
-      {task.title}
+      <div className="flex items-center gap-2">
+        {' '}
+        <label
+          className={`relative flex h-7 w-7 cursor-pointer items-center justify-center rounded-lg ${getStatusClasses()}`}
+        >
+          <input
+            type="checkbox"
+            checked={task.status === 'done'}
+            className="absolute h-full w-full cursor-pointer opacity-0"
+          />
+          {task.status === 'done' && <Check className="animate-bounce" />}
+          {task.status === 'in_progress' && <Loader className="animate-spin" />}
+          {task.status === 'not_started' && (
+            <Closed className="animate-pulse" />
+          )}
+        </label>
+        {task.title}
+      </div>
+      <a
+        href="#"
+        className="transition-opacity hover:animate-ping hover:opacity-80"
+      >
+        <Details />
+      </a>
     </div>
   )
 }
