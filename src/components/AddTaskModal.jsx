80be3991e5b52445d576ee4d7c1a3c54fedaf0b0
J27mendez <mendes_j27@hotmail.com>
2025-02-26 10:54:06 -0300

    feat: add SelectTime component

--- src/components/AddTaskModal.jsx
+++ src/components/AddTaskModal.jsx
@@ -6,7 +6,7 @@ import { CSSTransition } from 'react-transition-group'
 
 import Button from './Button'
 import Input from './Input'
-import InputLabel from './InputLabel'
+import SelectTime from './SelectTime'
 
 const AddTaskModal = ({ isOpen, handleClose }) => {
   const nodeRef = useRef()
@@ -33,17 +33,7 @@ const AddTaskModal = ({ isOpen, handleClose }) => {
               label="Titulo"
               placeholder={'Insira o titulo da tarefa'}
             />
-            <div className="flex flex-col gap-1 text-left">
-              <InputLabel htmlFor="time">Horário</InputLabel>
-              <select
-                id="time"
-                className="rounded-lg border border-solid border-[#ECECEC] px-4 py-3 outline-[#00AD85] placeholder:text-sm placeholder:text-[#9A9CF]"
-              >
-                <option value={'morning'}>Manhã</option>
-                <option value={'afternoon'}>Tarde</option>
-                <option value={'evening'}>Noite</option>
-              </select>
-            </div>
+            <SelectTime />
             <Input
               id="description"
               label="Descrição"
---
+++ src/components/SelectTime.jsx
@@ -0,0 +1,18 @@
+import InputLabel from './InputLabel'
+const SelectTime = () => {
+  return (
+    <div className="flex flex-col gap-1 text-left">
+      <InputLabel htmlFor="time">Horário</InputLabel>
+      <select
+        id="time"
+        className="rounded-lg border border-solid border-[#ECECEC] px-4 py-3 outline-[#00AD85] placeholder:text-sm placeholder:text-[#9A9CF]"
+      >
+        <option value={'morning'}>Manhã</option>
+        <option value={'afternoon'}>Tarde</option>
+        <option value={'evening'}>Noite</option>
+      </select>
+    </div>
+  )
+}
+
+export default SelectTime
