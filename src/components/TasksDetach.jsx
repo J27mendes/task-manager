0ca44570a26f36f8720e5ec58c53862bf789e90c
J27mendez <mendes_j27@hotmail.com>
2025-02-27 14:49:04 -0300

    chore: add prop-types to TaskDetach

--- src/components/TasksDetach.jsx
+++ src/components/TasksDetach.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types'
+
 const TasksDetach = ({ icon, text }) => {
   return (
     <div className="flex gap-2 border-b border-solid border-brend-divider pb-1">
@@ -5,6 +7,11 @@ const TasksDetach = ({ icon, text }) => {
       <p className="text-sm text-brend-time">{text}</p>
     </div>
   )
+}
+
+TasksDetach.prototype = {
+  text: PropTypes.string.isRequired,
+  icon: PropTypes.element.isRequired,
 }
 
 export default TasksDetach
