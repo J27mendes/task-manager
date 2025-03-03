8af7fdf02c066d14d42116134dcfb3de569b39e3
J27mendez <mendes_j27@hotmail.com>
2025-03-03 14:03:04 -0300

    feat: using useForm from react-hook-form  in AddTaskModal

--- src/components/AddTaskModal.jsx
+++ src/components/AddTaskModal.jsx
@@ -1,8 +1,9 @@
 import './AddTaskModal.css'
 
 import PropTypes from 'prop-types'
-import { useEffect, useRef, useState } from 'react'
+import { useRef, useState } from 'react'
 import { createPortal } from 'react-dom'
+import { useForm } from 'react-hook-form'
 import { CSSTransition } from 'react-transition-group'
 import { v4 } from 'uuid'
 
@@ -17,83 +18,61 @@ const AddTaskModal = ({
   onSubmitSuccess,
   onSubmitError,
 }) => {
-  const [errors, setErrors] = useState([])
   const [loading, setLoading] = useState(false)
   const nodeRef = useRef()
-  const titleRef = useRef()
-  const timeRef = useRef()
-  const descriptionRef = useRef()
-
-  useEffect(() => {
-    if (!isOpen) {
-      setErrors([])
-    }
-  }, [isOpen])
-
-  const handleSaveClick = async () => {
-    const title = titleRef.current.value
-    const time = timeRef.current.value
-    const description = descriptionRef.current.value
-
-    const newErrors = []
-    if (!title.trim()) {
-      newErrors.push({
-        inputName: 'title',
-        message: 'O titulo é obrigatório.',
-      })
-    }
-    if (!time.trim()) {
-      newErrors.push({
-        inputName: 'time',
-        message: 'Selecione o horário desejado.',
-      })
-    }
-    if (!description.trim()) {
-      newErrors.push({
-        inputName: 'description',
-        message: 'A descrição é obrigatória.',
-      })
-    }
-
-    setErrors(newErrors)
-    if (newErrors.length > 0) {
-      setLoading(false)
-      return
-    }
-    const bodyTask = {
-      id: v4(),
-      title: title.trim(),
-      time: time.trim(),
-      description: description.trim(),
-      status: 'not_started',
-    }
+  const {
+    register,
+    formState: { errors, isSubmitting },
+    handleSubmit,
+    reset,
+  } = useForm({
+    defaultValues: {
+      title: '',
+      time: 'morning',
+      description: '',
+    },
+  })
+  const handleSaveClick = async (data) => {
     setLoading(true)
     const response = await fetch('http://localhost:3000/TaskManager', {
       method: 'POST',
-      body: JSON.stringify(bodyTask),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        id: v4(),
+        title: data.title.trim(),
+        time: data.time,
+        description: data.description.trim(),
+        status: 'not_started',
+      }),
     })
 
     if (!response.ok) {
       setLoading(false)
       return onSubmitError()
     }
-
-    onSubmitSuccess({
-      id: v4(),
-      title,
-      time,
-      description,
-      status: 'not_started',
-    })
-    handleClose()
+    const result = await response.json()
+    onSubmitSuccess(result)
     setLoading(false)
+    handleClose()
+    reset({
+      defaultValues: {
+        title: '',
+        time: 'morning',
+        description: '',
+      },
+    })
   }
 
-  const titleError = errors.find((error) => error.inputName === 'title')
-  const timeError = errors.find((error) => error.inputName === 'time')
-  const descriptionError = errors.find(
-    (error) => error.inputName === 'description'
-  )
+  const handleCancelClick = () => {
+    reset({
+      title: '',
+      time: 'morning',
+      description: '',
+    })
+    handleClose()
+  }
 
   return createPortal(
     <CSSTransition
@@ -103,66 +82,87 @@ const AddTaskModal = ({
       classNames={'add-task-modal'}
       unmountOnExit
     >
-      <div
-        ref={nodeRef}
-        className="fixed inset-0 flex h-screen items-center justify-center backdrop-blur-sm"
-      >
-        <div className="rounded-xl bg-white p-5 text-center shadow-sm">
-          <h2 className="text-xl font-semibold text-brend-darkGrey">
-            Nova Tarefa
-          </h2>
-          <p className="mb-4 mt-1 text-sm text-brend-lightGrey">
-            Insira as informações abaixo
-          </p>
-          <div className="flex w-[336px] flex-col space-y-4">
-            <Input
-              label="Titulo"
-              id="title"
-              placeholder={'Insira o titulo da tarefa'}
-              errorMessage={titleError?.message}
-              ref={titleRef}
-              disabled={loading}
-            />
-            <SelectTime
-              errorMessage={timeError?.message}
-              ref={timeRef}
-              disabled={loading}
-            />
-            <Input
-              label="Descrição"
-              id="description"
-              placeholder={'Descrição da tarefa'}
-              errorMessage={descriptionError?.message}
-              ref={descriptionRef}
-              disabled={loading}
-            />
-            <div className="flex gap-3">
-              <Button
-                className="w-full"
-                color="secundary"
-                size="large"
-                onClick={handleClose}
-              >
-                Cancelar
-              </Button>
-              <Button
-                className="w-full"
-                size="large"
-                onClick={handleSaveClick}
-                disabled={loading}
-              >
-                Salvar
-                {loading && (
-                  <LoaderIcon
-                    className="ml-2 animate-spin text-brend-secundary"
-                    style={{ animationDuration: '2s' }}
-                  />
-                )}
-              </Button>
+      <form onSubmit={handleSubmit(handleSaveClick)}>
+        <div
+          ref={nodeRef}
+          className="fixed inset-0 flex h-screen items-center justify-center backdrop-blur-sm"
+        >
+          <div className="rounded-xl bg-white p-5 text-center shadow-sm">
+            <h2 className="text-xl font-semibold text-brend-darkGrey">
+              Nova Tarefa
+            </h2>
+            <p className="mb-4 mt-1 text-sm text-brend-lightGrey">
+              Insira as informações abaixo
+            </p>
+            <div className="flex w-[336px] flex-col space-y-4">
+              <Input
+                label="Titulo"
+                id="title"
+                placeholder={'Insira o titulo da tarefa'}
+                errorMessage={errors?.title?.message}
+                disabled={isSubmitting}
+                {...register('title', {
+                  required: 'O titulo é obrigatório',
+                  validate: (value) => {
+                    if (!value.trim()) {
+                      return 'O titulo não pode estar vázio'
+                    }
+                    return true
+                  },
+                })}
+              />
+              <SelectTime
+                errorMessage={errors?.time?.message}
+                disabled={isSubmitting}
+                {...register('time', {
+                  required: true,
+                })}
+              />
+              <Input
+                label="Descrição"
+                id="description"
+                placeholder={'Descrição da tarefa'}
+                errorMessage={errors?.description?.message}
+                disabled={isSubmitting}
+                {...register('description', {
+                  required: 'A descrição é obrigatória',
+                  validate: (value) => {
+                    if (!value.trim()) {
+                      return 'A descrição não pode estar vázia'
+                    }
+                    return true
+                  },
+                })}
+              />
+              <div className="flex gap-3">
+                <Button
+                  className="w-full"
+                  color="secundary"
+                  size="large"
+                  onClick={handleCancelClick}
+                  type="button"
+                >
+                  Cancelar
+                </Button>
+                <Button
+                  className="w-full"
+                  size="large"
+                  type="submit"
+                  disabled={isSubmitting}
+                >
+                  Salvar
+                  {loading && (
+                    <LoaderIcon
+                      className="ml-2 animate-spin text-brend-secundary"
+                      style={{ animationDuration: '2s' }}
+                    />
+                  )}
+                </Button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </form>
     </CSSTransition>,
     document.body
   )
