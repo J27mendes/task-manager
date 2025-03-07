4f8dff80dbb1585faf152af43bfa8a544163220f
J27mendez <mendes_j27@hotmail.com>
2025-03-07 14:35:58 -0300

    feat: add hook useHandleTaskStatusChange

---
+++ src/hooks/data/useHandleTaskStatusChange.js
@@ -0,0 +1,35 @@
+import { toast } from 'sonner'
+
+import { toastMessages } from '../../utils'
+import { useUpdateTask } from './useUpdateTask'
+
+export const useHandleTaskStatusChange = (tasks) => {
+  const { mutate, isPending } = useUpdateTask()
+
+  const handleTaskCheckboxChange = async (taskId) => {
+    const taskToUpdate = tasks?.find((task) => task.id === taskId)
+    if (!taskToUpdate) return
+
+    let newStatus
+    if (taskToUpdate.status === 'not_started') newStatus = 'in_progress'
+    else if (taskToUpdate.status === 'in_progress') newStatus = 'done'
+    else newStatus = 'not_started'
+
+    mutate(
+      { taskId, newStatus },
+      {
+        onSuccess: () => {
+          toast.success(toastMessages[newStatus].text, {
+            style: {
+              color: toastMessages[newStatus].color,
+              fontSize: '20px',
+              justifyContent: 'center',
+            },
+          })
+        },
+      }
+    )
+  }
+
+  return { handleTaskCheckboxChange, isPending }
+}
