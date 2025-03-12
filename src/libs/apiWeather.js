607caf0148675490162cbab27912bde037c7df7a
J27mendez <mendes_j27@hotmail.com>
2025-03-12 13:38:54 -0300

    refactor: using import.meta.env

--- src/libs/apiWeather.js
+++ src/libs/apiWeather.js
@@ -3,6 +3,6 @@ import axios from "axios"
 const apiKey = import.meta.env.VITE_APIKEY
 
 export const apiWeather = axios.create({
-  baseURL: "http://api.weatherapi.com/v1/",
+  baseURL: import.meta.env.VITE_WEATHER,
   params: { key: apiKey },
 })
